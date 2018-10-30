026a60876706651c41dde0a3250613c863328cf7
workstation1@DOMAIN.LOCAL <workstation1@DOMAIN.LOCAL>
2018-10-29 17:57:16 -0700

    basic layout and code

---
+++ logic.js
@@ -0,0 +1,31 @@
+// var config = {
+//     apiKey: "AIzaSyDm8cfSHDjdhjYkxO77oQqOM9RNxvZ1eVg",
+//     authDomain: "rpsgame-47199.firebaseapp.com",
+//     databaseURL: "https://rpsgame-47199.firebaseio.com",
+//     projectId: "rpsgame-47199",
+//     storageBucket: "rpsgame-47199.appspot.com",
+//     messagingSenderId: "979956531596"
+//   };
+//   firebase.initializeApp(config);
+//   database = firebase.database();
+
+$(document).ready(function(){
+
+    var game = {
+        elements: ["rock", "paper", "scissors"],
+        player1: {
+            name: "",
+            choice: ""
+        },
+        player2: {
+            name: "", 
+            choice: ""
+        }
+
+        resizeTo()
+
+        
+    }
+
+
+});
